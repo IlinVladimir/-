90fdf08d56945cec5842f9017da9d4119dbfdaed
Владимир Ильин <ilin.va@phystech.edu>
2026-09-24 13:31:31 +0300

    bin-counter

--- git/get-led/bin-counter.py
+++ git/get-led/bin-counter.py
@@ -5,3 +5,26 @@ GPIO.setmode(GPIO.BCM)
 leds = [16, 12, 25, 17, 27, 23, 22, 24]
 
 GPIO.setup(leds, GPIO.OUT)
+
+GPIO.output(leds, 0)
+
+
+
+num = 0
+
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+sleep_time = 0.2
+
+if GPIO.input(up):
+    num = num + 1
+    print(num, dec2bin(num))
+    time.sleep(sleep_time)
+
+if GPIO.input(up):
+    num = num + 1
+    print(num, dec2bin(num))
+    time.sleep(sleep_time)
+
+GPIO.output(leds, dec2bin(num))
